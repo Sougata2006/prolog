6c56d7bcfb89c48ca22934c5bfa781eb05a3e7e6
Sougata Paul <psougata32@gmail.com>
2026-05-15 11:04:24 +0530

    Rename family1.pl to grandparent.pl

--- family1.pl
+++
@@ -1,4 +0,0 @@
-parent(john,mary).
-parent(mary,sam).
-
-grandparent(X,Y):- parent(X,Z),parent(Z,Y).
---
+++ grandparent.pl
@@ -0,0 +1,4 @@
+parent(john,mary).
+parent(mary,sam).
+
+grandparent(X,Y):- parent(X,Z),parent(Z,Y).
